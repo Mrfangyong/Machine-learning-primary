fb98d64838a1fd28484aa0ffbd3e6fb6061ae67e
Ross He <rosshe@RossMBP.local>
2019-08-16 14:00:30 +0800

    线性回归项目代码上传

---
+++ Linear Regression/LinearRegression/Non-linearRegression.py
@@ -0,0 +1,29 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+
+from linear_regression import LinearRegression
+
+data = pd.read_csv('../data/non-linear-regression-x-y.csv')
+
+x = data['x'].values.reshape((data.shape[0], 1))
+y = data['y'].values.reshape((data.shape[0], 1))
+
+data.head(10)
+
+plt.plot(x, y)
+plt.show()
+
+num_iterations = 50000
+learning_rate = 0.02
+polynomial_degree = 15
+sinusoid_degree = 15
+normalize_data = True
+
+linear_regression = LinearRegression(x, y, polynomial_degree, sinusoid_degree, normalize_data)
+
+(theta, cost_history) = linear_regression.train(learning_rate, num_iterations)
+
+print('开始损失：{:.2f}'.format(cost_history[0]))
+print('结束损失：{:.2f}'.format(cost_history[-1]))
+
